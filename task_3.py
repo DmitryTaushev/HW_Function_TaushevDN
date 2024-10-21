aebb13db636eb4c4ce1959c5b4116fd551ed7ada
DmitryTaushev <123morfix@gmail.com>
2024-10-21 13:35:32 +0500

    Составили функцию которая выводит квадрат согласно полученным данным в task_3.py

--- task_3.py
+++ task_3.py
@@ -4,3 +4,21 @@
 # символ и переменную логического типа:
 # если она равна True, квадрат заполненный;
 # если False, квадрат пустой.
+
+def pic_square():
+    len = int(input("Введите длину стороны квадрата"))
+    symbol = input('Введите символ из которого будет состоять квадрат')
+    condition = input("Введите True - если квадрат должен быть заполненым, False, если пустым")
+    space = ' '
+    if condition == "True":
+        for i in range(len):
+            print(len*symbol)
+    elif condition == "False":
+        print(len * symbol)
+        for i in range(len-2):
+            print(symbol + space*(len-2) + symbol)
+        print(len * symbol)
+
+pic_square()
+
+
