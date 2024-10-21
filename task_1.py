93ccc7a4e4e41150146b08e75e132a761ec53ba7
DmitryTaushev <123morfix@gmail.com>
2024-10-21 13:12:05 +0500

    Внесли условие задачи в task_1.py

--- task_1.py
+++ task_1.py
@@ -1,3 +1,10 @@
+# Напишите функцию, которая отображает на экран
+# форматированный текст, указанный ниже:
+# “Don't compare yourself with anyone in this world…
+# if you do so, you are insulting yourself.”
+#  Bill Gates
+
+
 def textformat():
     print("“Don't compare yourself with anyone in this world…\nif you do so, you are insulting yourself.”\n\t\t\t\t\t\t\t\t\t\t Bill Gates")
 
