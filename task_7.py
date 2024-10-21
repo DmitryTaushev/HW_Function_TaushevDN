b6e9e338fc8c8a86c9968172116fde7ddd4508d4
DmitryTaushev <123morfix@gmail.com>
2024-10-21 14:57:32 +0500

    Составили функцию которая проверяет является ли число палиндромом в task_7.py

--- task_7.py
+++ task_7.py
@@ -6,3 +6,26 @@
 # палиндром(первая часть 123, вторая 321, которая после
 # переворота становится 123), 546645 — палиндром, а 421987 —
 # не палиндром.
+
+def palindrom(number):
+
+    if len(str(number)) % 2 != 0:
+        print("Число определенно не палиндром")
+    number_str = str(number)
+
+    count = len(number_str)
+    number_list = list(number_str)
+
+    count_1 = int(count / 2)
+    number_slice_1 = number_list[count_1:count]
+
+    number_1 = ''.join(number_list[0:count_1])
+    number_2 = ''.join(number_slice_1[::-1])
+
+    if number_1 == number_2:
+        print("Число палиндром")
+    else:
+        print("Число не палиндром")
+
+number = int(input("Введите число"))
+palindrom(number)
